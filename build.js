66e9577cb533a246b6f265f38c6f38db0100c532
Joe Fleming <joe.fleming@gmail.com>
2016-05-30 11:12:37 -0700

    handle redirects from old paths

--- build.js
+++ build.js
@@ -6,6 +6,7 @@ var helpers = require('metalsmith-discover-helpers');
 var collections = require('metalsmith-collections');
 var markdown = require('metalsmith-markdown');
 var dateFormatter = require('metalsmith-date-formatter');
+var redirect = require('metalsmith-redirect');
 var server = require('./lib/server');
 var sass = require('./lib/sass');
 var metallic = require('metalsmith-metallic');
@@ -48,6 +49,23 @@ var buildsteps = metalsmith(__dirname)
 buildsteps = layouts(buildsteps);
 
 buildsteps
+.use(redirect({
+  '/posts': '/all-posts',
+  '/posts/a-history-of-coding-and-computers': '/posts/my-history-of-coding-and-computers',
+  '/posts/configure-apache-for-multiple-wordpress-sites': '/posts/configuring-apache-and-fcgi-for-multiple-wordpress-sites',
+  '/posts/drupals-on-hold': '/posts/drupal-s-on-hold',
+  '/posts/evernotes-clearly': '/posts/evernote-s-clearly',
+  '/posts/grunt-painless-build-process': '/posts/grunt-for-a-painless-build-process',
+  '/posts/how-i-use-backbone-router': '/posts/how-i-use-backbone-s-router',
+  '/posts/ill-have-one-of-your-finest-arduinos-please': '/posts/i-ll-have-one-of-your-finest-arduinos-please',
+  '/posts/inaugural-openhack-postmortem': '/posts/inaugural-openhackphx-a-postmortem',
+  '/posts/openhack-002-portmortem': '/posts/openhackphx-002-a-postmortem',
+  '/posts/remote-backups-with-amazons-s3': '/posts/remote-backups-with-amazon-s-s3',
+  '/posts/skirting-isp-torrent-fitlering': '/posts/skirting-isp-torrent-filtering-reset-requests',
+  '/posts/slim-jade': '/posts/slim-jade-a-light-weight-php-framework-using-jade-templates',
+  '/posts/static-sites-rock': '/posts/static-websites-rock',
+  '/posts/wordpress-out-docpad-in': '/posts/wordpress-is-out-docpad-is-in',
+}))
 .build(function(err, files) {
   if (err) { throw err; }
   console.log('Build complete');
