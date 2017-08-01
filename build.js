7fa502e2cd547635eb1f4363f8dc13cd1ca331a3
Joe Fleming <joe.fleming@gmail.com>
2017-07-31 17:35:54 -0700

    feat: re-write build as composable functions

--- build.js
+++ build.js
@@ -1,54 +1,30 @@
-require('dotenv').load();
-
+const compose = require('just-compose');
 const metalsmith = require('metalsmith');
-const env = require('metalsmith-env');
-const filter = require('metalsmith-filter');
-const helpers = require('metalsmith-discover-helpers');
-const markdown = require('metalsmith-markdown');
-const dateFormatter = require('metalsmith-date-formatter');
-const snippet = require('metalsmith-snippet')
-const metallic = require('metalsmith-metallic');
-const server = require('./lib/server');
-const sass = require('./lib/sass');
-const collections = require('./lib/collections');
-const published = require('./lib/published');
-const permalinks = require('./lib/permalinks');
-const layouts = require('./lib/layouts');
-const feeds = require('./lib/feeds');
-const redirects = require('./lib/redirects');
-const metadata = require('./metadata');
 
 // metalsmith build process
-const buildsteps = metalsmith(__dirname)
-.metadata(metadata)
-.source('src')
-.destination('build')
-.use(env())
-.use(server())
-.use(filter(['**', '!**/.DS_Store']))
-.use(helpers({
-  directory: 'lib/helpers'
-}))
-.use(sass())
-.use(published())
-.use(collections())
-.use(metallic())
-.use(dateFormatter({
-  dates: [{
-    date: 'MMM Do, YYYY'
-  }]
-}))
-.use(markdown())
-.use(snippet({
-  maxLength: 300,
-  stripHtml: true,
-}))
-.use(permalinks());
-
-// wrap the output in layouts
-layouts(buildsteps)
-.use(feeds())
-.use(redirects())
+compose(
+  require('./lib/env'),
+  require('./lib/server'),
+  require('./lib/filter'),
+  require('./lib/handlebars_helpers'),
+  require('./lib/sass'),
+  require('./lib/published'),
+  require('./lib/collections'),
+  require('./lib/metallic'),
+  require('./lib/date_formatter'),
+  require('./lib/markdown'),
+  require('./lib/snippet'),
+  require('./lib/permalinks'),
+  require('./lib/layouts'),
+  require('./lib/feeds'),
+  require('./lib/redirects')
+)(
+  metalsmith(__dirname)
+  .metadata(require('./metadata'))
+  .source('src')
+  .destination('build')
+  .clean(true)
+)
 .build(function(err) {
   if (err) { throw err; }
   console.log('Build complete');
