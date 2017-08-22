86c55b913bb6cdfcfbe768315605fb9ee05fa01d
Joe Fleming <joe.fleming@gmail.com>
2017-08-21 17:35:17 -0700

    feat: re-org, create start script
    the whole app is moved around, and the scripts to run things have changed
    
    build - only run the build
    dev - run the build and host the files
    start - only host the files

--- dev-server.js
+++ dev-server.js
@@ -1,13 +1,4 @@
-const { createServer } = require('http');
-const { join } = require('path');
+const build = require('./lib/build');
+const server = require('./lib/server');
 
-const ecstatic = require('ecstatic');
-
-const port = process.env.PORT || 8080;
-const config = {
-  root: join(__dirname, 'build'),
-};
-
-createServer(ecstatic(config)).listen(port, () => {
-  console.log('Server listening on port', port);
-});
+build().then(() => server());
--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const compose = require('just-compose');
-const metalsmith = require('metalsmith');
+const server = require('./lib/server');
 
-// metalsmith build process
-compose(
-  require('./plugins/env'),
-  require('./plugins/filter'),
-  require('./plugins/handlebars_helpers'),
-  require('./plugins/sass'),
-  require('./plugins/published'),
-  require('./plugins/collections'),
-  require('./plugins/metallic'),
-  require('./plugins/date_formatter'),
-  require('./plugins/markdown'),
-  require('./plugins/snippet'),
-  require('./plugins/permalinks'),
-  require('./plugins/layouts'),
-  require('./plugins/feeds'),
-  require('./plugins/redirects')
-)(
-  metalsmith(__dirname)
-  .metadata(require('./metadata'))
-  .source('src')
-  .destination('build')
-  .clean(true)
-)
-.build(function(err) {
-  if (err) { throw err; }
-  console.log('Build complete');
-});
+server();
